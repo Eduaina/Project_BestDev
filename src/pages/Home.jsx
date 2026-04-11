91b74d26d5825b4ec9263af8b989b27e47e34ee0
Eduaina Ighalo <eduaighalo29@gmail.com>
2026-04-11 12:00:00 +0100

    Additions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 // again
 // Building differnt generic ideas and linking them all in this project. Again today
 // Winning everything... 100% on lockdown
-// Tired
+// Tired for just now but habdling business
 // Get your money up brokeyy
